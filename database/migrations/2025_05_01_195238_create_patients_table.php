4229730a17cada91b4ca218dab6d9ffeccaf15ef
valzza <valzadalipi2@gmail.com>
2025-05-01 22:31:06 +0200

    Ndrrimi databazes, MCR pacienti

---
+++ database/migrations/2025_05_01_195238_create_patients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('health_insurance_id'); //fk
+            $table->string('name');
+            $table->string('surname');
+            $table->date('birth_date');
+            $table->enum('gender', ['Mashkull', 'Femer']); 
+            $table->string('phone', 20);
+            $table->string('email');
+            $table->string('personal_id')->unique();
+            $table->string('emergency_contact_number', 20)->nullable();
+            $table->timestamps();
+
+            //per foreign key
+            // $table->foreign('health_insurance_id')->references('id')->on('health_insurance')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
