15c85d3454e9742bacadd0de2c32dddcddaeec03
vesaa-ubt <vh70445@ubt-uni.net>
2025-05-02 01:55:38 +0200

    Update 2025_05_01_222136_create_staff_table.php

--- database/migrations/2025_05_01_222136_create_staff_table.php
+++ database/migrations/2025_05_01_222136_create_staff_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('surname');
             $table->string('email')->unique();
-            $table->string('phone',20);
             $table->string('phone', 20);
             $table->string('role');
             $table->time('shift_start')->nullable();
