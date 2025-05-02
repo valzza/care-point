8a997b88bd60958f0ad82ccd3b4264ab6559bf60
vesaa-ubt <vh70445@ubt-uni.net>
2025-05-02 14:33:57 +0200

    Departament_Staff mcr

---
+++ app/Http/Controllers/DepartmentStaffController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Department_Staff;
+use Illuminate\Http\Request;
+
+class DepartmentStaffController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Department_Staff $department_Staff)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Department_Staff $department_Staff)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Department_Staff $department_Staff)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Department_Staff $department_Staff)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_05_02_122643_create_department__staff_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('department__staff', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('department_id'); //fk
+            $table->unsignedBigInteger('staff_id'); //fk
+            $table->timestamps();
+
+            //per foreign key
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('cascade');
+            $table->foreign('staff_id')->references('id')->on('staff')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('department__staff');
+    }
+};
