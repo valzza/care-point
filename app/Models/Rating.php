67e68bd1e18862958d14444e638c98680ea0fb76
jetamurtezi <jetamurtezi98@gmail.com>
2025-05-02 21:55:05 +0200

    Models Rating

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rating extends Model
 {
-    //
+    public function patient(){
+        return $this->belongsTo(Patient::class);
+    }
+    public function doctor(){
+        return $this->belongsTo(Doctor::class);
+    }
+
 }
