fb1ca89600e3051222256fc4eda9fb00ceb502e6
jetamurtezi <jetamurtezi98@gmail.com>
2025-05-02 22:01:43 +0200

    Models History

--- app/Models/History.php
+++ app/Models/History.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class History extends Model
 {
-    //
+    public function medical_card(){
+        return $this->belongsTo(MedicalCard::class);
+    }
+    public function test(){
+        return $this->hasMany(Test::class);
+    }
 }
